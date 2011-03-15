1ced6cb9bee45f7f93f4101c7aef0a33c641eb0c
David Nolen <david.nolen@gmail.com>
2011-03-15 10:01:14 -0400

    * src/bratko_logos/monkey_banana.clj: works

--- src/bratko_logos/monkey_banana.clj
+++ src/bratko_logos/monkey_banana.clj
@@ -1,29 +1,56 @@
 (ns bratko-logos.monkey-banana
-  (:require [logos.minikanren :as mk]))
+  (:require [logos.minikanren :as mk]
+            [clojure.contrib.macro-utils :as macro]))
 
-(defn walk [pos1 pos2]
-  (mk/cond-e
-   ((mk/== pos1 :door) (mk/== pos2 :window))
-   ((mk/== pos1 :window) (mk/== pos2 :door))))
-
-(defn move [before action after]
+(defn move-o [before action after]
   (mk/cond-e
    ((mk/== [:middle :onbox :middle :hasnot] before)
     (mk/== action :grasp)
     (mk/== [:middle :onbox :middle :has] after))
+   ((mk/exist [pos has]
+              (mk/== [pos :onfloor pos has] before)
+              (mk/== :climb action)
+              (mk/== [pos :onbox pos has] after)))
+   ((mk/exist [pos1 pos2 box has]
+              (mk/== [pos1 :onfloor pos1 has] before)
+              (mk/== :push action)
+              (mk/== [pos2 :onfloor pos2 has] after)))
    ((mk/exist [pos1 pos2 box has]
               (mk/== [pos1 :onfloor box has] before)
-              (mk/== (walk pos1 pos2) action)
+              (mk/== :walk action)
               (mk/== [pos2 :onfloor box has] after)))))
 
+(defn canget-o [state out]
+  (macro/symbol-macrolet [_ (mk/lvar)]
+   (mk/cond-e
+    ((mk/== [_ _ _ :has] state) (mk/== out true))
+    ((mk/exist [action next]
+               (move state action next)
+               (canget-o next out))))))
+
 (comment
-  (defn-e walk [pos1 pos2]
-    ([:door :window])
-    ([:window :door]))
+
+  ;; (true)
+  (mk/run 1 [q]
+          (canget-o [:atdoor :onfloor :atwindow :hasnot] q))
+
+  ;; challenge collect the steps
+
+  ;; With pattern matching we can write the following
 
   (defn-e move [before action after]
-    ([[:middle :onbox :middle :hasnot] :grasp [:middle :onbox :middle :has]])
-    ([[?pos1 :onfloor ?box ?has] _ [?pos2 :onfloor ?box ?has]]))
+    ([[:middle :onbox :middle :hasnot]
+      :grasp
+      [:middle :onbox :middle :has]])
+    ([[?pos :onfloor ?pos ?has]
+      :climb
+      [?pos :onbox ?pos ?has]])
+    ([[?pos1 :onfloor ?pos1 ?has]
+      :push
+      [?pos2 :onfloor ?pos2 ?has]])
+    ([[?pos1 :onfloor ?box ?has]
+      :walk
+      [?pos2 :onfloor ?box ?has]]))
 
   (mk/run 1 [q]
           (move [:middle :onbox :middle :hasnot] :grasp q))

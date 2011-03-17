8a58eba4a904323d267a8a6de1c8817a5a53718f
David Nolen <david.nolen@gmail.com>
2011-03-17 01:54:23 -0400

    * project.clj: new pattern matching world.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject bratko-logos "0.1.0-SNAPSHOT"
   :description "Bratko's Prolog Programming for AI in miniKanren"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0-alpha5"]
                  [logos "0.5.2-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
--- src/bratko_logos/monkey_banana.clj
+++ src/bratko_logos/monkey_banana.clj
@@ -1,7 +1,8 @@
 (ns bratko-logos.monkey-banana
-  (:require [logos.minikanren :as mk]))
+  (:require [logos.minikanren :as mk]
+            [logos.match :as m]))
 
-(mk/defn-e move-o [before action after]
+(m/defn-e move-o [before action after]
   ([[:middle :onbox :middle :hasnot]
     :grasp
     [:middle :onbox :middle :has]])
@@ -15,13 +16,13 @@
     :walk
     [?pos2 :onfloor ?box ?has]]))
 
-(defn-e canget-o [state out]
+(m/defn-e canget-o [state out]
   ([[_ _ _ :has] true])
-  ([_ _] (exist [action next]
-                (move-o state action next)
-                (canget-o next out))))
+  ([_ _] (mk/exist [action next]
+                   (move-o state action next)
+                   (canget-o next out))))
 
 (comment
- (run 1 [q]
+ (mk/run 1 [q]
       (canget-o [:atdoor :onfloor :atwindow :hasnot] q))
  )

150ad77b076db5aa46adae68eb684fcfa3a5db35
David Nolen <david.nolen@gmail.com>
2011-04-03 11:34:39 -0400

    * project.clj: update logos dep version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject bratko-logos "0.1.0-SNAPSHOT"
   :description "Bratko's Prolog Programming for AI in miniKanren"
   :dependencies [[org.clojure/clojure "1.3.0-alpha5"]
-                 [logos "0.5.2-SNAPSHOT"]]
+                 [logos "0.6.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])

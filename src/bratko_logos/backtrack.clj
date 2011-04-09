457b36c7ba22e35b2202e41a1856fb9332ee06d1
David Nolen <david.nolen@gmail.com>
2011-04-09 13:32:01 -0400

    * src/bratko_logos/backtrack.clj: update using new arithmetic goals

--- src/bratko_logos/backtrack.clj
+++ src/bratko_logos/backtrack.clj
@@ -1,33 +1,19 @@
 (ns bratko-logos.backtrack
   (:refer-clojure :exclude [reify inc ==])
   (:use [logos minikanren match])
-  (:require [logos.nonrel :as n]
-            [logos.arithmetic :as a]))
+  (:require [logos.arithmetic :as a]))
 
 (defn-e f [x y]
-  ([_ 0] (n/project [x]
-                    (== (< x 3) true)))
-  ([_ 2] (n/project [x]
-                    (== (<= 3 x) true)
-                    (== (< x 6) true)))
-  ([_ 4] (n/project [x]
-                    (== (<= 6 x) true))))
-
-(comment
-  ;; preferred syntax
-  (defn-e f [x y]
     ([_ 0] (a/< x 3))
     ([_ 2] (a/<= 3 x) (a/< x 6))
     ([_ 4] (a/<= 6 x)))
-  )
 
 (comment
   ;; fails
   ;; but it tries other branches pointlessly
   (run 1 [y]
        (f 1 y)
-       (n/project [y]
-                  (== (< 2 y) true)))
+       (a/< 2 y))
 
   ;; cond-a
   )

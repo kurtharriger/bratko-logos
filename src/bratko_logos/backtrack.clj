54dcc9def76d583158c75368e143b5cc8c01ed91
David Nolen <david.nolen@gmail.com>
2011-04-09 15:46:34 -0400

    * src/bratko_logos/backtrack.clj: use logging to show that all clauses are tried unnecessarily

--- src/bratko_logos/backtrack.clj
+++ src/bratko_logos/backtrack.clj
@@ -8,12 +8,22 @@
     ([_ 2] (a/<= 3 x) (a/< x 6))
     ([_ 4] (a/<= 6 x)))
 
+(defn-e f-log [x y]
+    ([_ 0] (log "clause 1") (a/< x 3))
+    ([_ 2] (log "clause 2") (a/<= 3 x) (a/< x 6))
+    ([_ 4] (log "clause 3") (a/<= 6 x)))
+
 (comment
   ;; fails
   ;; but it tries other branches pointlessly
   (run 1 [y]
        (f 1 y)
        (a/< 2 y))
+
+  ;; we can see that here
+  (run-debug 1 [y]
+             (f-log 1 y)
+             (a/< 2 y))
 
   ;; cond-a
   )

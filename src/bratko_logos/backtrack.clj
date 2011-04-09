bd2c43ae3a75aa893c96b520c98970807632de4a
David Nolen <david.nolen@gmail.com>
2011-04-09 16:03:47 -0400

    * src/bratko_logos/backtrack.clj: rename fns

--- src/bratko_logos/backtrack.clj
+++ src/bratko_logos/backtrack.clj
@@ -4,17 +4,17 @@
         [logos.nonrel :only [defn-a]])
   (:require [logos.arithmetic :as a]))
 
-(defn-e f [x y]
+(defn-e fa [x y]
     ([_ 0] (a/< x 3))
     ([_ 2] (a/<= 3 x) (a/< x 6))
     ([_ 4] (a/<= 6 x)))
 
-(defn-e f-log-a [x y]
+(defn-e fb [x y]
     ([_ 0] (log "clause 1") (a/< x 3))
     ([_ 2] (log "clause 2") (a/<= 3 x) (a/< x 6))
     ([_ 4] (log "clause 3") (a/<= 6 x)))
 
-(defn-a f-log-b [x y]
+(defn-a fc [x y]
     ([_ 0] (log "clause 1") (a/< x 3))
     ([_ 2] (log "clause 2") (a/<= 3 x) (a/< x 6))
     ([_ 4] (log "clause 3") (a/<= 6 x)))
@@ -23,17 +23,17 @@
   ;; fails
   ;; but it tries other branches pointlessly
   (run 1 [y]
-       (f 1 y)
+       (fa 1 y)
        (a/< 2 y))
 
   ;; we can see that here
   (run-debug 1 [y]
-             (f-log-a 1 y)
+             (fb 1 y)
              (a/< 2 y))
 
   ;; cond-a
   (run-debug 1 [y]
-             (f-log-b 1 y)
+             (fc 1 y)
              (a/< 2 y))
   )
   

c6487cb95bfbfa52f4c55d1c6175d5f7d81bf0ba
David Nolen <david.nolen@gmail.com>
2011-04-09 16:01:54 -0400

    * src/bratko_logos/backtrack.clj: show how to use cond-a

--- src/bratko_logos/backtrack.clj
+++ src/bratko_logos/backtrack.clj
@@ -1,6 +1,7 @@
 (ns bratko-logos.backtrack
   (:refer-clojure :exclude [reify inc ==])
-  (:use [logos minikanren match])
+  (:use [logos minikanren match]
+        [logos.nonrel :only [defn-a]])
   (:require [logos.arithmetic :as a]))
 
 (defn-e f [x y]
@@ -8,7 +9,12 @@
     ([_ 2] (a/<= 3 x) (a/< x 6))
     ([_ 4] (a/<= 6 x)))
 
-(defn-e f-log [x y]
+(defn-e f-log-a [x y]
+    ([_ 0] (log "clause 1") (a/< x 3))
+    ([_ 2] (log "clause 2") (a/<= 3 x) (a/< x 6))
+    ([_ 4] (log "clause 3") (a/<= 6 x)))
+
+(defn-a f-log-b [x y]
     ([_ 0] (log "clause 1") (a/< x 3))
     ([_ 2] (log "clause 2") (a/<= 3 x) (a/< x 6))
     ([_ 4] (log "clause 3") (a/<= 6 x)))
@@ -22,9 +28,12 @@
 
   ;; we can see that here
   (run-debug 1 [y]
-             (f-log 1 y)
+             (f-log-a 1 y)
              (a/< 2 y))
 
   ;; cond-a
+  (run-debug 1 [y]
+             (f-log-b 1 y)
+             (a/< 2 y))
   )
   
